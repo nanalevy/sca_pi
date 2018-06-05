3daeab1f2d8451b88c4660d5da763cf63d5cb0b5
Levy <gevidarar@dumoac.net>
2018-06-05 15:52:31 -0500

    more scripts

---
+++ boolean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [$1 -gt 100 ]
+then
+	echo "Thats a large number"
+fi
+	echo Your number was $1
+
---
+++ input.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "What are your favorite fruits?"
+read fruit1 fruit2 fruit3
+
+echo Your first fruit was $fruit1
+echo Your second fruit was $fruit2
+echo Your third fruit was $fruit3
